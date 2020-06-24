33abd82f02691a94fa5056f2fbe3dd4d14f96a41
mmuffins <mail@tiv73.com>
2020-06-24 18:16:40 +0200

    Add unit tests

--- ArtistNormalizer.API/Persistence/Contexts/AppDbContext.cs
+++ ArtistNormalizer.API/Persistence/Contexts/AppDbContext.cs
@@ -17,11 +17,11 @@ namespace ArtistNormalizer.API.Persistence.Contexts
             builder.Entity<Artist>().HasKey(p => p.Id);
             builder.Entity<Artist>().Property(p => p.Id).IsRequired().ValueGeneratedOnAdd();
 
-            builder.Entity<Artist>().HasData
-            (
-                new Artist { Id = 100, Name = "Artist 1" }, // Id set manually due to in-memory provider
-                new Artist { Id = 101, Name = "Artist 2" }
-            );
+            //builder.Entity<Artist>().HasData
+            //(
+            //    new Artist { Id = 100, Name = "Artist 1" }, // Id set manually due to in-memory provider
+            //    new Artist { Id = 101, Name = "Artist 2" }
+            //);
         }
     }
 }
--- Tests/IntegrationTests.cs
+++ Tests/IntegrationTests.cs
@@ -1,41 +1,112 @@
 ﻿using ArtistNormalizer.API;
+using ArtistNormalizer.API.Domain.Models;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http;
 using System.Text;
+using System.Text.Json;
 using System.Threading.Tasks;
 using Xunit;
 
 namespace Tests
 {
-    public class PlayersControllerIntegrationTests : IClassFixture<CustomWebApplicationFactory<Startup>>
+    [Collection("Sequential")]
+    public class IntegrationTests : IClassFixture<CustomWebApplicationFactory<Startup>>
     {
-        private readonly HttpClient _client;
+        private readonly HttpClient client;
 
-        public PlayersControllerIntegrationTests(CustomWebApplicationFactory<Startup> factory)
+        public IntegrationTests(CustomWebApplicationFactory<Startup> factory)
         {
-            _client = factory.CreateClient();
+            client = factory.CreateClient();
         }
 
         [Fact]
-        public async Task CanGetPlayers()
+        public async Task Artist_Post()
         {
-            // The endpoint or route of the controller action.
-            var httpResponse = await _client.GetAsync("/api/artist");
+            var endpoint = "/api/artist";
 
-            // Must be successful.
+            var initialResponse = await client.GetAsync(endpoint);
+            initialResponse.EnsureSuccessStatusCode();
+
+            var initialCount = JsonSerializer.Deserialize<IEnumerable<Artist>>(await initialResponse.Content.ReadAsStringAsync());
+            Assert.Empty(initialCount);
+
+            // Add test data
+            var artList = new List<Artist>();
+
+            artList.Add(new Artist()
+            {
+                Name = "New Artist 1"
+            });
+
+            artList.Add(new Artist()
+            {
+                Name = "New Artist 2"
+            });
+
+
+            var JsonOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
+
+            foreach (var art in artList)
+            {
+                var jsonString = new StringContent(JsonSerializer.Serialize(new { Name = art.Name }), Encoding.UTF8, "application/json");
+                var postResponse = await client.PostAsync(endpoint, jsonString);
+                postResponse.EnsureSuccessStatusCode();
+
+                var postResponseObj = JsonSerializer.Deserialize<Artist>(await postResponse.Content.ReadAsStringAsync(), JsonOptions);
+                Assert.Equal(art.Name, postResponseObj.Name);
+            }
+
+            // Verify
+            var httpResponse = await client.GetAsync(endpoint);
             httpResponse.EnsureSuccessStatusCode();
 
             // Deserialize and examine results.
-            var stringResponse = await httpResponse.Content.ReadAsStringAsync();
-            //var players = JsonConvert.DeserializeObject<IEnumerable<Player>>(stringResponse);
-            //Assert.Contains(players, p => p.FirstName == "Wayne");
-            //Assert.Contains(players, p => p.FirstName == "Mario");
+            var verifyList = JsonSerializer.Deserialize<IEnumerable<Artist>>(await httpResponse.Content.ReadAsStringAsync(), JsonOptions);
+
+            Assert.Collection(verifyList, item => Assert.Contains(artList[0].Name, item.Name),
+                                            item => Assert.Contains(artList[1].Name, item.Name));
         }
 
+        [Fact]
+        public async Task Artist_Remove()
+        {
+            var endpoint = "/api/artist";
+            var initialResponse = await client.GetAsync(endpoint);
+            initialResponse.EnsureSuccessStatusCode();
 
+            var JsonOptions = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true,
+            };
 
+            var initialArtist = JsonSerializer.Deserialize<IEnumerable<Artist>>(await initialResponse.Content.ReadAsStringAsync(), JsonOptions).ToList();
+            Assert.Equal(2, initialArtist.Count());
 
+            var deleteArtist = initialArtist[0];
+            var keepArtist = initialArtist[1];
+
+            var deleteResponse = await client.DeleteAsync($"{endpoint}/{deleteArtist.Id}");
+            deleteResponse.EnsureSuccessStatusCode();
+
+            var deleteResponseObj = JsonSerializer.Deserialize<Artist>(await deleteResponse.Content.ReadAsStringAsync(), JsonOptions);
+            Assert.Equal(deleteArtist.Id, deleteResponseObj.Id);
+
+            // Verify
+            var httpResponse = await client.GetAsync(endpoint);
+            httpResponse.EnsureSuccessStatusCode();
+
+            // Deserialize and examine results.
+            var verifyList = JsonSerializer.Deserialize<IEnumerable<Artist>>(await httpResponse.Content.ReadAsStringAsync(), JsonOptions).ToList();
+
+            Assert.Single(verifyList);
+            Assert.Equal(keepArtist.Id, verifyList.First().Id);
+        }
 
     }
 }
+
