d9b7d72d8cb3c64ed9e176f670f97f679cbaeb6a
mmuffins <mail@tiv73.com>
2024-05-08 14:47:38 +0200

    Update json object

--- ArtistNormalizer.API/Resources/MbArtistResource.cs
+++ ArtistNormalizer.API/Resources/MbArtistResource.cs
@@ -6,16 +6,13 @@ namespace ArtistNormalizer.API.Resources
     {
         public int Id { get; set; }
         public string MbId { get; set; }
-        public string Type { get; set; }
         public string Name { get; set; }
         public string OriginalName { get; set; }
-        public string SortName { get; set; }
-        public string CustomName { get; set; }
         public bool Include { get; set; }
 
         public override string ToString()
         {
-            return CustomName;
+            return Name;
         }
     }
 }
