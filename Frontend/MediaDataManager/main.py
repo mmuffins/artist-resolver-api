df72f8bdb18562a2ac8f1f221aa01b1d5efb42ea
mmuffins <mail@tiv73.com>
2024-05-09 20:18:44 +0200

    save files

--- Frontend/MediaDataManager/main.py
+++ Frontend/MediaDataManager/main.py
@@ -4,8 +4,9 @@ import os
 import asyncio
 import tkinter as tk
 from typing import List, Optional
-from mutagen.id3 import ID3
-
+from mutagen import id3
+# TODO: datetime is just for debuggin purposes, remove it
+from datetime import datetime
 
 class Alias:
 	def __init__(self, name: str, type: str, locale: Optional[str], begin: Optional[str], end: Optional[str], type_id: str, ended: bool, sort_name: str, primary: bool):
@@ -107,13 +108,24 @@ class TrackDetails:
 		
 	async def read_file_metadata(self) -> None:
 		loop = asyncio.get_event_loop()
-		self.id3 = await loop.run_in_executor(None, lambda: ID3(self.file_path))
+		self.id3 = await loop.run_in_executor(None, lambda: id3.ID3(self.file_path))
 		self.title = self.id3.get('TIT2',[''])[0]
 		self.artist = self.id3.get('TPE1', [''])[0]
 		self.album = self.id3.get('TALB', [''])[0]
 		
 		artist_relations = ([frame for frame in self.id3.getall("TXXX") if frame.desc == 'artist_relations_json'])[0].text[0]
 		self.mbArtistDetails = await self.manager.parse_mbartist_json(artist_relations)
+
+	def save_file_metadata(self) -> None:
+		file_changed: bool = False
+
+		self.id3["TSRC"] = id3.TSRC(encoding=3, text=datetime.now().strftime("%H-%M"))
+		
+		if self.artist:
+			self.id3["TEXT"] = id3.TEXT(encoding=3, text=self.artist)
+
+		if file_changed:
+			self.id3.save(self.file_path)
 
 class MediaDataManager:
 	MBARTIST_API_ENDPOINT = "api/mbartist"
@@ -136,6 +148,10 @@ class MediaDataManager:
 					file_path = os.path.join(root, file)
 					self.tracks.append(TrackDetails(file_path, self))
 	
+	async def save_files(self) -> None:
+		loop = asyncio.get_event_loop()
+		await asyncio.gather(*(loop.run_in_executor(None, track.save_file_metadata) for track in self.tracks))
+
 	async def read_file_metadata(self) -> None:
 		await asyncio.gather(*(track.read_file_metadata() for track in self.tracks))
 
@@ -241,6 +257,7 @@ async def main() -> None:
 	dir = "C:/Users/email_000/Desktop/music/sample/spiceandwolf"
 	await manager.load_directory(dir)
 	await manager.persist_mbartist_customizations()
+	await manager.save_files()
 
 if __name__ == "__main__":
 	asyncio.run(main())
